07631722d421ed578f63cf73126a9685cd6077c8
ChaseKlingel3 <klingelchase@gmail.com>
2016-01-21 14:08:25 -0800

    Create CheckNums.js

---
+++ CheckNums.js
@@ -0,0 +1,14 @@
+function CheckNums(num1,num2) { 
+  // Check if num2 is greater than num1.
+	if (num2 > num1) {
+	    // If it is, return the string "true".
+      return "true";
+  // Check if num2 is less than num1.
+  } else if (num2 < num1) {
+      // If it is, return the string "false".
+      return "false";
+  // If previous tests failed, they must be equal and I return -1.
+  } else {
+      return -1;
+    }      
+}
