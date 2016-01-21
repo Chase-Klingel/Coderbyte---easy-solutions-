2da7467aa666077fdf7b4eda676e2e2f5f2e528e
ChaseKlingel3 <klingelchase@gmail.com>
2016-01-21 13:22:24 -0800

    Update SimpleAdding.js

--- SimpleAdding.js
+++ SimpleAdding.js
@@ -2,7 +2,7 @@ function SimpleAdding(num) {
   // I declare var sum that will end up equalling 1 + ... num.
   var sum = 0;
   
-  // I create a for loop that is initialized at 1 and continues until i <= num, increasing i by 1 on each pass.
+  // I declare a for loop that is initialized at 1 and terminates when i <= num and I increase i by 1 on each pass.
   for (var i = 1; i <= num; i++) {
       // on each pass, I add i to sum.
       sum += i;
