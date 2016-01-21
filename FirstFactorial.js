01186be23862ba7275aa5ed17dfd6bb9bc116a65
ChaseKlingel3 <klingelchase@gmail.com>
2016-01-21 13:21:43 -0800

    Update FirstFactorial.js

--- FirstFactorial.js
+++ FirstFactorial.js
@@ -1,7 +1,6 @@
 function FirstFactorial(num) { 
-   // I declare a for loop that is initialized at num - 1.
-   // the for loop stops when i = 2.
-   // i decreases by 1 on each pass.
+   
+   // I declare a for loop that is initialized at num - 1 and terminates when i = 2 and I increase i by 1 on each pass.
    for (var i = num - 1; i > 1; i--) {
       // multiply num * i 
       num *= i;
