9fd473f1696e47af1efc8411d43d19ce2075553d
ChaseKlingel3 <klingelchase@gmail.com>
2016-01-21 14:39:30 -0800

    Update WordCount.js

--- WordCount.js
+++ WordCount.js
@@ -3,9 +3,9 @@ function WordCount(str) {
   var numWords = 0;
   
   // I declare a var str and split the str into an array of individual words. 
-	var str = str.split(" ");
+  var str = str.split(" ");
 	
-	// I declare a for loop that will pass over each individual word in the str param being passed...
+  // I declare a for loop that will pass over each individual word in the str param being passed...
   for (var i = 0; i < str.length; i++) {
       // on each pass, I update numWords by 1.
       numWords++;
