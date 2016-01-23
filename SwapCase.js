9928474b0c009c9d7dd2ceb529991aa038663a71
ChaseKlingel3 <klingelchase@gmail.com>
2016-01-22 17:05:54 -0800

    Update SwapCase.js

--- SwapCase.js
+++ SwapCase.js
@@ -1,12 +1,19 @@
 function SwapCase(str) {
-  // 
+  // I declare a var answer and initialize it to an emptry string. 
   var answer = "";
-	for (var i = 0; i < str.length; i++) {
+  
+  // I declare a for loop that will pass over each character in the string...
+  for (var i = 0; i < str.length; i++) {
+      // if current character is uppercase
       if (str[i] === str[i].toUpperCase()) {
+      	// I call the .toLowerCase() method on the current character and add it to var answer.
         answer += str[i].toLowerCase();
+      // Otherwise, if current character is lowercase
       } else if (str[i] === str[i].toLowerCase()) {
+      	// I call the .toUpperCase() method on the current character and add it to var answer.
         answer += str[i].toUpperCase();
       }
     }
-  	return answer;
+    // Finally, I return the answer. 
+    return answer;
 }
