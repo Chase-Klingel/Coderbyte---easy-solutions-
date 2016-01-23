6be6e79cf48e240af3109443042b723312a5210d
ChaseKlingel3 <klingelchase@gmail.com>
2016-01-22 18:38:15 -0800

    Create ThirdGreatest.js

---
+++ ThirdGreatest.js
@@ -0,0 +1,6 @@
+function ThirdGreatest(strArr) { 
+  // First, I sort the strArr from largest to smallest. 
+	strArr = strArr.sort(function(a, b) { return b.length - a.length; });
+	// Then, I simply return the word contained in index 2 because this is now the third largest word in strArr.
+  return strArr[2];
+}
