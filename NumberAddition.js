df51e90b84eae090bf99ff6570fc1b8de89a546b
ChaseKlingel3 <klingelchase@gmail.com>
2016-01-22 18:07:47 -0800

    Update NumberAddition.js

--- NumberAddition.js
+++ NumberAddition.js
@@ -1,9 +1,9 @@
 function NumberAddition(str) { 
   // I declare a var arr and use a regex expression to create an array of numbers.
   // the asterik ensures that multi-digit numbers are grouped together in one single index of the array.
-	var arr = str.match(/[0-9]*/g);
-	
-	// I declare a var total and initialize it to 0.
+  var arr = str.match(/[0-9]*/g);
+  
+  // I declare a var total and initialize it to 0.
   var total = 0;
   
   // I declare a for loop that passes over each number in the array...
